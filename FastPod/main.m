b850084f46e728dcc430e44a1482a9b04920f83e
PonyCui <cuiminghui@yy.com>
2017-01-08 16:12:27 +0800

    Initial Commit

---
+++ FastPod/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FastPod
+//
+//  Created by 崔 明辉 on 2017/1/8.
+//  Copyright © 2017年 Pony Cui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
